90883ffd35a362e7d71b10d24d6248d29d47ba5f
jeraldgan <jeraldgantc@gmail.com>
2022-07-13 19:47:17 +0800

    Removed cors issue

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,5 +1,7 @@
-Moralis.initialize("304B71svY6bTC8doQS3bWDZXaiaaNqY1WVKhv9mb");
-Moralis.serverUrl = "https://acy4vcywgg7l.usemoralis.com:2053/server";
+const appId = "304B71svY6bTC8doQS3bWDZXaiaaNqY1WVKhv9mb";
+const serverUrl = "https://acy4vcywgg7l.usemoralis.com:2053/server";
+
+Moralis.start({ serverUrl, appId });
 
 async function initialiseApp() {
   let currentUser = Moralis.User.current();
